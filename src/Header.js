16cf2928f9a24cbfb6db088ca963d060fd1512a1
unknown <sh102201@seoul.ac.kr>
2020-10-18 11:46:24 +0900

    adfa

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import {Link, useHistory } from "react-router-dom";
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="title">breeze</div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Swipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TinderCard from 'react-tinder-card';
+
+
+function swipe() {
+    return (
+        <div>
+            <TinderCard></TinderCard>
+        </div>
+    )
+}
+
+export default swipe
